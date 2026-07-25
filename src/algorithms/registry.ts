da23827fa4914242c6148948edee4b1e06279580
Onur Seckin Senoglu <onur@proxai.co>
2026-07-25 01:46:51 -0700

    feat: integrate algorithms from competitive programming handbook desktop pdf

--- src/algorithms/registry.ts
+++ src/algorithms/registry.ts
@@ -9,31 +9,30 @@ import { quickSort } from './two_pointers/quickSort';
 import { validParentheses } from './stack/validParentheses';
 import { binarySearchMatrix } from './binary_search/binarySearchMatrix';
 import { slidingWindowMin } from './sliding_window/slidingWindowMin';
+import { reverseLinkedList } from './linked_list/reverseLinkedList';
 import { binaryTreeLca } from './trees/binaryTreeLca';
 import { triePrefixTree } from './tries/triePrefixTree';
+import { kthLargestElement } from './heap/kthLargestElement';
 import { nQueens } from './backtracking/nQueens';
 import { bfsGraph } from './graphs/bfsGraph';
 import { numberOfIslands } from './graphs/numberOfIslands';
 import { coinChangeDp } from './dp_1d/coinChangeDp';
-import { editDistance } from './dp_2d/editDistance';
+import { mergeIntervals } from './intervals/mergeIntervals';
+import { huffmanCoding } from './greedy/huffmanCoding';
 import { dijkstraShortestPath } from './advanced_graphs/dijkstraShortestPath';
 import { topologicalSort } from './advanced_graphs/topologicalSort';
 import { kruskalMst } from './advanced_graphs/kruskalMst';
 import { bellmanFord } from './advanced_graphs/bellmanFord';
-import { fenwickTree } from './advanced_range_and_cp/fenwickTree';
-import { segmentTree } from './advanced_range_and_cp/segmentTree';
-import { kmpStringMatch } from './advanced_range_and_cp/kmpStringMatch';
+import { editDistance } from './dp_2d/editDistance';
 import { sievePrimes } from './math_and_geometry/sievePrimes';
 import { nimGame } from './math_and_geometry/nimGame';
 import { convexHull } from './math_and_geometry/convexHull';
-import { kthLargestElement } from './heap/kthLargestElement';
 import { countingBits } from './bit_manipulation/countingBits';
-import { mergeIntervals } from './intervals/mergeIntervals';
-import { huffmanCoding } from './greedy/huffmanCoding';
-import { reverseLinkedList } from './linked_list/reverseLinkedList';
+import { fenwickTree } from './advanced_range_and_cp/fenwickTree';
+import { segmentTree } from './advanced_range_and_cp/segmentTree';
+import { kmpStringMatch } from './advanced_range_and_cp/kmpStringMatch';
 
 export const ALGORITHM_REGISTRY: Record<string, AlgorithmDefinition> = {
-  'reverse-linked-list': reverseLinkedList as AlgorithmDefinition,
   'prefix-sum': prefixSum as AlgorithmDefinition,
   'two-sum': twoSum as AlgorithmDefinition,
   'kadane-max-subarray': kadaneMaxSubarray as AlgorithmDefinition,
@@ -44,27 +43,28 @@ export const ALGORITHM_REGISTRY: Record<string, AlgorithmDefinition> = {
   'valid-parentheses': validParentheses as AlgorithmDefinition,
   'binary-search-matrix': binarySearchMatrix as AlgorithmDefinition,
   'sliding-window-min': slidingWindowMin as AlgorithmDefinition,
+  'reverse-linked-list': reverseLinkedList as AlgorithmDefinition,
   'binary-tree-lca': binaryTreeLca as AlgorithmDefinition,
   'trie-prefix-tree': triePrefixTree as AlgorithmDefinition,
+  'kth-largest-element': kthLargestElement as AlgorithmDefinition,
   'n-queens': nQueens as AlgorithmDefinition,
   'bfs-graph': bfsGraph as AlgorithmDefinition,
   'number-of-islands': numberOfIslands as AlgorithmDefinition,
   'coin-change-dp': coinChangeDp as AlgorithmDefinition,
-  'edit-distance': editDistance as AlgorithmDefinition,
+  'merge-intervals': mergeIntervals as AlgorithmDefinition,
+  'huffman-coding': huffmanCoding as AlgorithmDefinition,
   'dijkstra-shortest-path': dijkstraShortestPath as AlgorithmDefinition,
   'topological-sort': topologicalSort as AlgorithmDefinition,
   'kruskal-mst': kruskalMst as AlgorithmDefinition,
   'bellman-ford': bellmanFord as AlgorithmDefinition,
-  'fenwick-tree': fenwickTree as AlgorithmDefinition,
-  'segment-tree': segmentTree as AlgorithmDefinition,
-  'kmp-string-match': kmpStringMatch as AlgorithmDefinition,
+  'edit-distance': editDistance as AlgorithmDefinition,
   'sieve-primes': sievePrimes as AlgorithmDefinition,
   'nim-game': nimGame as AlgorithmDefinition,
   'convex-hull': convexHull as AlgorithmDefinition,
-  'kth-largest-element': kthLargestElement as AlgorithmDefinition,
   'counting-bits': countingBits as AlgorithmDefinition,
-  'merge-intervals': mergeIntervals as AlgorithmDefinition,
-  'huffman-coding': huffmanCoding as AlgorithmDefinition,
+  'fenwick-tree': fenwickTree as AlgorithmDefinition,
+  'segment-tree': segmentTree as AlgorithmDefinition,
+  'kmp-string-match': kmpStringMatch as AlgorithmDefinition,
 };
 
 export const getAlgorithm = (id: string): AlgorithmDefinition | undefined => {
