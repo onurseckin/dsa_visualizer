a848f51e63ba66f956e85dd23c3bd379b138d5e3
onurseckin <onurseckinsenoglu@gmail.com>
2026-07-27 06:19:25 -0700

    fix: update UnionFind find method to use recursive path compression

--- src/algorithms/graph_spanning_trees/kruskalMst/pythonCode.ts
+++ src/algorithms/graph_spanning_trees/kruskalMst/pythonCode.ts
@@ -4,9 +4,10 @@ export const KRUSKAL_CODE = `class UnionFind:
         self.rank = [1] * size
 
     def find(self, x):
-        while x != self.root[x]:
-            x = self.root[x]
-        return x
+        if self.root[x] == x:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
 
     def union(self, x, y):
         rootX = self.find(x)
